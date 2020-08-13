ade0c0531fd8c6cfa57eac63292de5c6b870aeed
Tran Phuoc Hung <ba@mocvien.com>
2020-08-13 15:35:36 +0700

    add

---
+++ WebTBrowser/WebTBrowser/MainTWebViewController.h
@@ -0,0 +1,17 @@
+//
+//  MainTWebViewController.h
+//  WebTBrowser
+//
+//  Created by Hung Tran on 8/13/20.
+//  Copyright © 2020 Tran Phuoc Hung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainTWebViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WebTBrowser/WebTBrowser/MainTWebViewController.m
@@ -0,0 +1,32 @@
+//
+//  MainTWebViewController.m
+//  WebTBrowser
+//
+//  Created by Hung Tran on 8/13/20.
+//  Copyright © 2020 Tran Phuoc Hung. All rights reserved.
+//
+
+#import "MainTWebViewController.h"
+
+@interface MainTWebViewController ()
+
+@end
+
+@implementation MainTWebViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
